1c4f97f77eafabed41d05f216ab13b5241284a4b
jeheecheon <jeheecheon@gmail.com>
2025-05-10 01:11:46 +0900

    feat: install eslint-plugin-prettier

--- typescript/packages/eslint-config/base.ts
+++ typescript/packages/eslint-config/base.ts
@@ -1,13 +1,13 @@
 import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 import turbo from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 
 export const config = tseslint.config(
   js.configs.recommended,
-  eslintConfigPrettier,
   tseslint.configs.recommended,
   turbo.configs["flat/recommended"],
+  eslintPluginPrettierRecommended,
   {
     ignores: ["dist/**"],
   }
