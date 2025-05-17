aff06a01b7bd230631cfd2ad3c53a111765e8438
jeheecheon <jeheecheon@gmail.com>
2025-05-17 17:04:36 +0900

    feat: install prettier-plugin-tailwindcss

--- typescript/apps/fe-blog/prettier.config.mjs
+++ typescript/apps/fe-blog/prettier.config.mjs
@@ -1,3 +1,3 @@
-import config from "@packages/prettier-config/base";
+import config from "@packages/prettier-config/front";
 
 export default config;
---
+++ typescript/packages/prettier-config/front.mjs
@@ -0,0 +1,6 @@
+/**
+ * @type {import("prettier").Config}
+ */
+export default {
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
+};
