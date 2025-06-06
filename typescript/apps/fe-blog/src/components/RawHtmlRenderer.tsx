d022c150d9b8c7506547bf40a62ad8b393be4f26
jeheecheon <jeheecheon@gmail.com>
2025-06-06 17:09:43 +0900

    refactor: enhance RawHtmlRenderer

--- typescript/apps/fe-blog/src/components/RawHtmlRenderer.tsx
+++ typescript/apps/fe-blog/src/components/RawHtmlRenderer.tsx
@@ -1,16 +1,28 @@
-import parse, { type HTMLElement } from "node-html-parser";
+import parse, { HTMLElement, TextNode, type Node } from "node-html-parser";
 import { VoidComponent, type JSX } from "solid-js";
+import { Dynamic } from "solid-js/web";
 import Image from "~/components/Image";
 
 const RawHtmlRenderer: VoidComponent<{ rawHtml: string }> = (props) => {
-  return renderToJsx(parse(props.rawHtml));
+  return renderToJSX(parse(props.rawHtml));
 
-  function renderToJsx(node: HTMLElement): JSX.Element {
-    if (node.rawTagName === "img") {
+  function renderToJSX(node: HTMLElement | Node): JSX.Element {
+    if (node instanceof HTMLElement && node.rawTagName === "img") {
       return <Image {...node.attributes} />;
     }
 
-    return node.children.map(renderToJsx);
+    if (node instanceof TextNode) {
+      return node.rawText;
+    }
+
+    return (
+      <Dynamic
+        component={node.rawTagName || "div"}
+        {...(node instanceof HTMLElement ? node.attributes : {})}
+      >
+        {node.childNodes.map(renderToJSX)}
+      </Dynamic>
+    );
   }
 };
 
