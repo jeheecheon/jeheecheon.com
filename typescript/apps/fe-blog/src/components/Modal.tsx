266e2c4ee6de8dce384f0e3b1167dd9fb0e178c1
jeheecheon <jeheecheon@gmail.com>
2025-06-15 14:37:54 +0900

    feat: add title and size props to Modal

--- typescript/apps/fe-blog/src/components/Modal.tsx
+++ typescript/apps/fe-blog/src/components/Modal.tsx
@@ -1,5 +1,7 @@
+import { xMark } from "solid-heroicons/solid";
 import { ParentComponent } from "solid-js";
 import { Portal } from "solid-js/web";
+import Icon from "~/components/Icon";
 import PresenceTransition from "~/components/PresenceTransition";
 import { useKeydown } from "~/hooks/useKeydown";
 import { useLockBodyScroll } from "~/hooks/useLockBodyScroll";
@@ -7,6 +9,8 @@ import { cn } from "~/utils/class-name";
 
 const Modal: ParentComponent<{
   class?: string;
+  title: string;
+  size: "sm" | "md" | "lg";
   visible: boolean;
   onClose: () => void;
 }> = (props) => {
@@ -21,15 +25,35 @@ const Modal: ParentComponent<{
         visible={props.visible}
       >
         <div
-          class="fixed inset-0 flex min-h-screen min-w-screen items-center justify-center bg-black/50"
+          class="fixed inset-0 z-50 overflow-y-auto bg-black/50 p-4"
           onClick={props.onClose}
         >
-          <section
-            class={cn("bg-white p-4", props.class)}
-            onClick={handleContentClick}
-          >
-            {props.children}
-          </section>
+          <div class="flex min-h-full items-center justify-center">
+            <section
+              class={cn(
+                "h-fit w-full rounded-2xl bg-zinc-900 p-4 outline outline-offset-4 outline-orange-300",
+                props.size === "sm" && "max-w-sm",
+                props.size === "md" && "max-w-md",
+                props.size === "lg" && "max-w-lg",
+                props.class,
+              )}
+              onClick={handleContentClick}
+            >
+              <div class="flex items-center justify-between">
+                <h2 class="text-2xl font-bold text-orange-200">
+                  {props.title}
+                </h2>
+                <button
+                  class="transition-transform duration-700 hover:scale-125"
+                  onClick={props.onClose}
+                >
+                  <Icon class="size-6 text-white" path={xMark} />
+                </button>
+              </div>
+
+              <div class="mt-4">{props.children}</div>
+            </section>
+          </div>
         </div>
       </PresenceTransition>
     </Portal>
