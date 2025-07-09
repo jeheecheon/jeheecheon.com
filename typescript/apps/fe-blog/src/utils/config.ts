04db40b6b802bbbe083ef5eeee0a221cdbf14da8
jeheecheon <jeheecheon@gmail.com>
2025-07-10 02:49:29 +0900

    fix: use VITE_ prefixed env

--- typescript/apps/fe-blog/src/utils/config.ts
+++ typescript/apps/fe-blog/src/utils/config.ts
@@ -1,6 +1,6 @@
 export const configs = {
-  NODE_ENV: process.env.NODE_ENV,
-  BLOG_API_URL: process.env.BLOG_API_URL || "http://localhost:4001",
+  NODE_ENV: import.meta.env.NODE_ENV,
+  BLOG_API_URL: import.meta.env.VITE_BLOG_API_URL || "http://localhost:4001",
   BLOG_GRAPHQL_URL:
-    process.env.BLOG_GRAPHQL_URL || "http://localhost:4001/graphql",
+    import.meta.env.VITE_BLOG_GRAPHQL_URL || "http://localhost:4001/graphql",
 };
