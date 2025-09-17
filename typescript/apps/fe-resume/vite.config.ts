5a13de619c4cf1344fdee0c56f1ebb4780768d32
jeheecheon <jeheecheon@gmail.com>
2025-09-17 20:54:05 +0900

    fix: resolve absolute path

--- typescript/apps/fe-resume/vite.config.ts
+++ typescript/apps/fe-resume/vite.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
@@ -6,5 +7,10 @@ export default defineConfig({
   plugins: [tailwindcss(), solidPlugin()],
   server: {
     port: 4002,
+  },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "src"),
+    },
   },
 });
