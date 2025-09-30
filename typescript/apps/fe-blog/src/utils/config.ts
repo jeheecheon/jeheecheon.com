c1c455a9383bfb4171f479cd2241a168e3c21b8f
jeheecheon <jeheecheon@gmail.com>
2025-10-01 01:15:52 +0900

    chore: remove unused env

--- typescript/apps/fe-blog/src/utils/config.ts
+++ typescript/apps/fe-blog/src/utils/config.ts
@@ -1,5 +1,4 @@
 export const configs = {
-  NODE_ENV: import.meta.env.NODE_ENV,
   BLOG_API_URL: import.meta.env.VITE_BLOG_API_URL || "http://localhost:4001",
   BLOG_GRAPHQL_URL:
     import.meta.env.VITE_BLOG_GRAPHQL_URL || "http://localhost:4001/graphql",
