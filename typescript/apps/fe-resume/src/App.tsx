c32f5b2901a7a428004e75e90528b175b714e212
jeheecheon <jeheecheon@gmail.com>
2025-12-01 21:07:10 +0900

    feat: add content to resume

--- typescript/apps/fe-resume/src/App.tsx
+++ typescript/apps/fe-resume/src/App.tsx
@@ -1,20 +1,16 @@
-import Button from "@packages/ui/components/Button.jsx";
-import Spinner from "@packages/ui/components/Spinner";
-import Textarea from "@packages/ui/components/Textarea";
-import type { Component } from "solid-js";
-
+import Container from "@packages/ui/components/Container";
+import Paper from "@packages/ui/components/Paper";
 import "@packages/ui/styles/globals-base.css";
+import type { Component } from "solid-js";
 
 const App: Component = () => {
   return (
-    <div class="flex h-screen w-screen items-center justify-center">
-      <Button theme="primary" size="lg">
-        Hello World
-      </Button>
-      <Spinner class="absolute-center size-10 bg-red-500" />
-      <Textarea value="asd" />
-      <div class="text-red-500">Hello World</div>
-    </div>
+    <Container>
+      <Paper class="py-base min-h-screen" as="main">
+        <h1 class="py-3 text-2xl font-bold text-orange-300">Resume.</h1>
+        <p>My resume is private for now.</p>
+      </Paper>
+    </Container>
   );
 };
 
--- typescript/apps/fe-resume/vite.config.ts
+++ typescript/apps/fe-resume/vite.config.ts
@@ -1,7 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [tailwindcss(), solidPlugin()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "src"),
+    },
+  },
 });
