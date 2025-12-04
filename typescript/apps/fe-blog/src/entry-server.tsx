30e8ed2c1d85f4d447170543538cada4493ae5ab
jeheecheon <jeheecheon@gmail.com>
2025-12-04 23:54:28 +0900

    chore: add ga tag

--- typescript/apps/fe-blog/src/entry-server.tsx
+++ typescript/apps/fe-blog/src/entry-server.tsx
@@ -6,6 +6,20 @@ export default createHandler(() => (
     document={({ assets, children, scripts }) => (
       <html lang="en">
         <head>
+          {/* Google tag (gtag.js) */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-S7CHEX469G"
+          />
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-S7CHEX469G');
+            `}
+          </script>
+
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
