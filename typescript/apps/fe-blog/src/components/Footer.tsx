7498b1d940a5a89027231611893cea1f359f5bb6
jeheecheon <jeheecheon@gmail.com>
2025-12-05 02:28:38 +0900

    feat: update Footer and ConditionalLink components for dynamic copyright year and external link support

--- typescript/apps/fe-blog/src/components/Footer.tsx
+++ typescript/apps/fe-blog/src/components/Footer.tsx
@@ -2,10 +2,12 @@ import ConditionalLink from "@packages/ui/components/ConditionalLink";
 import Container from "@packages/ui/components/Container";
 import Paper from "@packages/ui/components/Paper";
 import { cn } from "@packages/ui/utils/class-name";
-import { VoidComponent } from "solid-js";
+import { createMemo, VoidComponent } from "solid-js";
 import { configs } from "~/utils/config";
 
 const Footer: VoidComponent<{ class?: string }> = (props) => {
+  const year = createMemo(() => new Date().getFullYear());
+
   return (
     <footer class={cn("", props.class)}>
       <Container>
@@ -13,11 +15,12 @@ const Footer: VoidComponent<{ class?: string }> = (props) => {
           <ConditionalLink
             class="block text-center text-sm text-zinc-400 hover:text-orange-300"
             href={`mailto:${configs.EMAIL}`}
+            isExternal
           >
-            jeheecheon@gmail.com
+            {configs.EMAIL}
           </ConditionalLink>
           <p class="mt-2 text-center text-sm text-zinc-400">
-            Copyright 2025 Jehee Cheon. All rights reserved.
+            Copyright {year()} Jehee Cheon. All rights reserved.
           </p>
         </Paper>
       </Container>
--- typescript/packages/ui/src/components/ConditionalLink.tsx
+++ typescript/packages/ui/src/components/ConditionalLink.tsx
@@ -5,10 +5,15 @@ import { Dynamic } from "solid-js/web";
 type Props = {
   class?: string;
   href?: string;
+  isExternal?: boolean;
 } & JSX.AnchorHTMLAttributes<HTMLAnchorElement>;
 
 const ConditionalLink: ParentComponent<Props> = (props) => {
-  const isExternal = createMemo(() => /^\/.+/.test(props.href || ""));
+  const isExternal = createMemo(() =>
+    typeof props.isExternal === "boolean"
+      ? props.isExternal
+      : /^\/.+/.test(props.href || ""),
+  );
 
   return (
     <Dynamic
